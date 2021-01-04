5d78f84a951cc9545589c65b2127754045ac4b78
naveben <naveb@gmail.com>
2021-01-04 23:44:28 +0200

    protocol finish commented

--- src/main/java/bgu/spl/net/impl/BGUSERVER/MessagingProtocolUser.java
+++ src/main/java/bgu/spl/net/impl/BGUSERVER/MessagingProtocolUser.java
@@ -8,111 +8,109 @@ import bgu.spl.net.impl.rci.*;
 public class MessagingProtocolUser implements MessagingProtocol<CSCommand> {
     private boolean shouldTerminate=false;
     private final Database database= Database.getInstance();
-    private User CurrentUserLogin= null;
+    private User CurrentLoggedInUser = null;  // referance to the logged User
+
     public CSCommand process(CSCommand msg) {
-            String[] CmdArgs = msg.getArgs();
-            // what the command and what the field
+            String[] CmdArgs = msg.getArgs(); // arguments of the command
             short opcode = msg.getOpcode();
-            CSCommand cs= null;
+            CSCommand replyCommand= null;
             try{
             switch (opcode) {
                 //Admin Register
                 case 1:
-                    if (CurrentUserLogin!=null) throw new MyServerError("already login");
+                    if (CurrentLoggedInUser != null) throw new MyServerError("already logged in");
                     database.Register(TypeOfUser.Admin, CmdArgs[0], CmdArgs[1]);
-                    //send ACK 1
-                    cs = new ACKimp(opcode);
+                    replyCommand = new ACKimp(opcode); //ACK 1
                     break;
                 //Student Register
                 case 2:
-                    if (CurrentUserLogin!=null) throw new MyServerError("already login");
+                    if (CurrentLoggedInUser !=null) throw new MyServerError("already logged in");
                     database.Register(TypeOfUser.Student, CmdArgs[0], CmdArgs[1]);
-                    cs = new ACKimp(opcode);
+                    replyCommand = new ACKimp(opcode); //ACK 2
                     break;
                 //Login
                 case 3:
-                    CurrentUserLogin = database.Login(CmdArgs[0], CmdArgs[1]);
-                    cs = new ACKimp(opcode);
+                    CurrentLoggedInUser = database.Login(CmdArgs[0], CmdArgs[1]); //return this logged in user if Login was successfully
+                    replyCommand = new ACKimp(opcode); //ACK 3
                     break;
-                    //Logout
+                //Logout
                 case 4:
-                    if (CurrentUserLogin == null) throw new MyServerError("not login");
-                    database.Logout(CurrentUserLogin);
-                    CurrentUserLogin = null;
-                    shouldTerminate=false;
-                    cs = new ACKimp(opcode);
+                    if (CurrentLoggedInUser == null) throw new MyServerError("not logged in");
+                    database.Logout(CurrentLoggedInUser);
+                   //  CurrentLoggedInUser = null; // remove current logged in user //TODO: delete
+                    shouldTerminate=true;
+                    replyCommand = new ACKimp(opcode); //ACK 4
                     break;
                 //COURSEREG
                 case 5:
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Admin)
-                        throw new MyServerError("not login or the user is not allowed to do so (because Admin)");
-                    database.RegisterCourse(CurrentUserLogin, Integer.parseInt(CmdArgs[0]));
-                    cs = new ACKimp(opcode);
+                    if (CurrentLoggedInUser == null || CurrentLoggedInUser.getType() == TypeOfUser.Admin)
+                        throw new MyServerError("not logged in or the user is not allowed"); // not allowed because Admin
+                    database.RegisterCourse(CurrentLoggedInUser, Integer.parseInt(CmdArgs[0]));
+                    replyCommand = new ACKimp(opcode); // ACK 5
                     break;
                 //KDAMCHECK
                 case 6:
-                    //TODO:: maybe Admin can request Kdam check as well, need to check
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Admin)
-                        throw new MyServerError("not login or the user is not allowed to do so (because Admin)");
+                    if (CurrentLoggedInUser == null)
+                        throw new MyServerError("not logged in");
                     else {
                         String KdamCheck = database.getKdamCheckList(Integer.parseInt(CmdArgs[0]));
-                        cs = new ACKimp(opcode);
-                        cs.SetArgument2(KdamCheck);
+                        replyCommand = new ACKimp(opcode); //ACK 6
+                        replyCommand.SetArgument2(KdamCheck);
                     }
                     break;
                 //COURSESTAT
                 case 7:
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Student)
-                        throw new MyServerError("not login or the user is not allowed to do so (because student)");
+                    if (CurrentLoggedInUser == null || CurrentLoggedInUser.getType() == TypeOfUser.Student)
+                        throw new MyServerError("not login or the user is not allowed");  // not allowed because Student
                     else {
-                        String Stat = database.CourseStat(Integer.parseInt(CmdArgs[0])); //return
-                        cs = new ACKimp(opcode);
-                        cs.SetArgument2(Stat);
+                        String Stat = database.CourseStat(Integer.parseInt(CmdArgs[0]));
+                        replyCommand = new ACKimp(opcode); //ACK 7
+                        replyCommand.SetArgument2(Stat);
                     }
                     break;
                 //STUDENTSTAT
                 case 8:
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Student)
-                        throw new MyServerError("not login or the user is not allowed to do so (because student)");
+                    if (CurrentLoggedInUser == null || CurrentLoggedInUser.getType() == TypeOfUser.Student)
+                        throw new MyServerError("not login or the user is not allowed");  // not allowed because Student
                     else {
                         String Stat = database.StudentStat(CmdArgs[0]);
-                        cs = new ACKimp(opcode);
-                        cs.SetArgument2(Stat);
+                        replyCommand = new ACKimp(opcode); //ACK 8
+                        replyCommand.SetArgument2(Stat);
                     }
                     break;
-                //  ISREGISTERED
+                //ISREGISTERED
                 case 9:
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Admin)
-                        throw new MyServerError("not login or the user is not allowed to do so (because admin)");
+                    if (CurrentLoggedInUser == null || CurrentLoggedInUser.getType() == TypeOfUser.Admin)
+                        throw new MyServerError("not login or the user is not allowed"); //not allowed because Admin
                     else {
                         String Message = "NOT REGISTERED";
-                        if (database.IsRegisteredtoCoruse(CurrentUserLogin, Integer.parseInt(CmdArgs[0])))
+                        if (database.IsRegisteredtoCoruse(CurrentLoggedInUser, Integer.parseInt(CmdArgs[0])))
                             Message = "REGISTERED"; // send back some how
-                        cs = new ACKimp(opcode);
-                        cs.SetArgument2(Message);
+                        replyCommand = new ACKimp(opcode); //ACK 9
+                        replyCommand.SetArgument2(Message);
                     }
                     break;
-                    //UNREGISTER
+                //UNREGISTER
                 case 10:
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Admin)
-                        throw new MyServerError("not login or the user is not allowed to do so (because admin)");
+                    if (CurrentLoggedInUser == null || CurrentLoggedInUser.getType() == TypeOfUser.Admin)
+                        throw new MyServerError("not login or the user is not allowed"); //not allowed because Admin
                     else {
-                        database.UnRegisterCourse(CurrentUserLogin, Integer.parseInt(CmdArgs[0]));
-                        cs = new ACKimp(opcode);
+                        database.UnRegisterCourse(CurrentLoggedInUser, Integer.parseInt(CmdArgs[0]));
+                        replyCommand = new ACKimp(opcode); //ACK 10
                     }
                     break;
                 //MYCOURSES
                 case 11:
-                    if (CurrentUserLogin == null || CurrentUserLogin.getType() == TypeOfUser.Admin)
-                        throw new MyServerError("not login or the user is not allowed to do so (because admin)");
-                    String Message = database.ListOfCoursesStudentRegisteredOrdered(CurrentUserLogin);
-                    cs = new ACKimp(opcode);
-                    cs.SetArgument2(Message);
+                    if (CurrentLoggedInUser == null || CurrentLoggedInUser.getType() == TypeOfUser.Admin)
+                        throw new MyServerError("not login or the user is not allowed"); //not allowed because Admin
+                    String Message = database.ListOfCoursesStudentRegisteredOrdered(CurrentLoggedInUser);
+                    replyCommand = new ACKimp(opcode); //ACK 11
+                    replyCommand.SetArgument2(Message);
                     break;
             }}catch(MyServerError e){
-                cs= new Errorimp(opcode);
+                replyCommand= new Errorimp(opcode); // if error is thrown up return ERROR according to relevant opcode
             }
-            return cs;}
+            return replyCommand;}
 
             public boolean shouldTerminate() {
                 return shouldTerminate;

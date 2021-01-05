e3237d79e111715fcfc733877060547830e11fb5
IGitRev95 <idorev@post.bgu.ac.il>
2021-01-05 03:21:19 +0200

    Server init01

--- src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -42,7 +42,6 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
                     }
                 }
             }
-            close();
         } catch (IOException ex) {
             ex.printStackTrace();
         }

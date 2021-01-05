31a21c5ac59529137ec93d215dcacdcb5877c385
IGitRev95 <idorev@post.bgu.ac.il>
2021-01-05 02:58:56 +0200

    Server init01

--- src/main/java/bgu/spl/net/impl/BGRSServer/ReactorMain.java
+++ src/main/java/bgu/spl/net/impl/BGRSServer/ReactorMain.java
@@ -6,7 +6,7 @@ import bgu.spl.net.srv.Server;
 public class ReactorMain {
     public static void main (String[] args) {
         Database.getInstance(); //just fot initialize
-        Server server = Server.reactor(Integer.parseInt(args[0]),Integer.parseInt(args[1]), () -> new MessagingProtocolUser(), () -> new EncoderDecoderBGU());
+        Server server = Server.reactor(Integer.parseInt(args[1]),Integer.parseInt(args[0]), () -> new MessagingProtocolUser(), () -> new EncoderDecoderBGU());
         server.serve();
     }
 }
